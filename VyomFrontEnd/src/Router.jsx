9b04c17056630f07d5e4d8c90a5ed65109c6c534
Prantick Deb <prantickdeb2005@gmail.com>
2025-11-19 23:08:27 +0530

    Corrected tha author router from slug to documentId

--- VyomFrontEnd/src/Router.jsx
+++ VyomFrontEnd/src/Router.jsx
@@ -4,22 +4,77 @@ import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Post from "./pages/Post";
 import Author from "./pages/Author";
+import Search from "./pages/Search";
 import About from "./pages/About";
+
+
 import Layout from "./components/layout/Layout";
 
-export default function Router(){
+const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Layout><Home /></Layout>} />
+        
+        
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
 
-        <Route path="/post/:slug" element={<Layout><Post /></Layout>} />
-        <Route path="/category/:slug" element={<Layout><Category /></Layout>} />
-        <Route path="/author/:slug" element={<Layout><Author /></Layout>} />
-        <Route path="/about" element={<Layout><About /></Layout>} />
+        <Route
+          path="/post/:slug"
+          element={
+            <Layout>
+              <Post />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/category/:slug"
+          element={
+            <Layout>
+              <Category />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/author/:documentId"
+          element={
+            <Layout>
+              <Author />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <Search />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <About />
+            </Layout>
+          }
+        />
+
+        
+
       </Routes>
     </BrowserRouter>
   );
 };
 
-
+export default AppRouter;
