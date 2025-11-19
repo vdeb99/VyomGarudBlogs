f7f974f6e281ddf3492ba3523ad230a131ecce26
Prantick Deb <prantickdeb2005@gmail.com>
2025-11-19 23:03:57 +0530

    Added posts by author api

--- VyomFrontEnd/src/api/authors.js
+++ VyomFrontEnd/src/api/authors.js
@@ -1,5 +1,6 @@
 import { api } from "./client";
 
-export const getAuthorById = (id) =>
-  api.get(`/authors/${id}?populate=*`);
+export const getAuthorByDocId = (docId) => api.get(`/authors/${docId}?populate=*`);
 
+export const getPostsByAuthor = (authorDocId) =>
+  api.get(`/posts?filters[author][documentId][$eq]=${authorDocId}&populate=*`);
