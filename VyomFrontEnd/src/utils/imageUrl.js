2e0269ac4c84ed30e9f2d559c5c77f7e9b81aa12
Prantick Deb <prantickdeb2005@gmail.com>
2025-11-19 19:33:05 +0530

    fixed image util

--- VyomFrontEnd/src/utils/imageUrl.js
+++ VyomFrontEnd/src/utils/imageUrl.js
@@ -1,6 +1,7 @@
+
 export default function imageUrl(img){
     if(!img){
         return "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg";
     }
-    return import.meta.env.VITE_STRAPI_URL + img.url
+    return import.meta.env.VITE_STRAPI_URL+img
 }
