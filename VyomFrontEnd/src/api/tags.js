ac714d3a6ce42569bcb5d8e9c1037c411dbf2116
Prantick Deb <prantickdeb2005@gmail.com>
2025-11-23 00:48:53 +0530

    Removed tag api

--- VyomFrontEnd/src/api/tags.js
+++
@@ -1,7 +0,0 @@
-// import { api } from "./client";
-
-// export const getAllTags = () =>
-//   api.get("/tags");
-
-// export const getTagBySlug = (slug) =>
-//   api.get(`/tags?filters[slug][$eq]=${slug}&populate=posts`);
