31cbbbb04ea4a378546a55d2c486ba7270ac92af
Prantick Deb <prantickdeb2005@gmail.com>
2025-11-23 14:48:58 +0530

    Modified loader component

--- VyomFrontEnd/src/components/ui/Loader.jsx
+++ VyomFrontEnd/src/components/ui/Loader.jsx
@@ -1,7 +1,10 @@
+import {LoaderCircle} from 'lucide-react';
 const Loader = () => {
     return (
       <div className="flex justify-center items-center py-10">
-        <div className="w-10 h-10 border-t-4 border-blue-600 border-solid rounded-full animate-spin"></div>
+        <div className="w-10 h-10 border-t-4 border-blue-600 border-solid rounded-full animate-spin">
+          <LoaderCircle className="w-10 h-10 text-blue-600" />
+        </div>
       </div>
     );
   };
