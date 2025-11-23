bbcd7fd44db857d852c76e24e7aac4115f4171a7
Prantick Deb <prantickdeb2005@gmail.com>
2025-11-23 18:47:14 +0530

    Update url fetching for deployement

--- VyomFrontEnd/src/utils/imageUrl.js
+++ VyomFrontEnd/src/utils/imageUrl.js
@@ -1,7 +1,13 @@
+export default function imageUrl(img) {
+  if (!img) {
+    return "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg";
+  }
 
-export default function imageUrl(img){
-    if(!img){
-        return "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg";
-    }
-    return import.meta.env.VITE_STRAPI_URL+img
+  if (img.startsWith("http") || img.startsWith("//")) {
+    return img;
+  }
+
+  const strapiUrl = import.meta.env.VITE_STRAPI_URL;
+
+  return `${strapiUrl}${img}`;
 }
